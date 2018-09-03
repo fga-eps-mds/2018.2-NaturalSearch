10ae391a9aae60599c62e8bfe95c477101e49655
zero101010 <igotaraujo97@gmail.com>
2018-09-03 10:40:02 -0300

    add to the settings 2 allowed hosts

--- NaturalSearch/settings.py
+++ NaturalSearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bxa1fqj^f7inealzip9e!+ce#==t*53kswebxe8(4zsm)wc1q8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
 
 
 # Application definition
