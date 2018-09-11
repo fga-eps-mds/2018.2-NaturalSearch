24d7b249cb335b4cf5ee07ad96f17208449ba15d
zero101010 <igotaraujo97@gmail.com>
2018-09-11 17:08:01 -0300

    Adds structure of template in home.html Add urls.py and update views with method home

--- NaturalSearch/urls.py
+++ NaturalSearch/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from natural_search.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name='home')
 ]
